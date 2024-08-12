8ecda4b136f5c2f0dd021035587225222517ae32
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2024-08-12 05:14:28 -0500

    Moved some stuff
    Changed codes out

--- modules/commands/purge.js
+++ modules/commands/purge.js
@@ -1,26 +1,10 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { PermissionFlagsBits } = require('discord-api-types/v10');
-module.exports = {  
-	data: new SlashCommandBuilder()
-		.setName('purge')
-		.setDescription('Purges Messages from chat!')
-        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages | PermissionFlagsBits.ManageRoles)
-        .addIntegerOption(option => {
-            return option
-            .setName('amount')
-            .setDescription('Up to 100')
-            .setRequired(true)
-        }),
-        execute(interaction){
-            if (interaction.commandName === 'purge') {
-            }
+module.exports = {
+    purge: function (command) {
+        const options = command
+        const channelId = command
+         client.on('interactionCreate',async (interaction) => {
+            interaction.isCommand() && main.commands[interaction.commandName] ? main.commands[interaction.commandName](interaction, client) : false
+            command.permissions == 'MANAGE_MESSAGES' ? (client.channels.fetch(channelId).then(channel => (channel.bulkDelete(options.getNumber('number') || 100, true)).then(command.reply({ephemeral: true})))) : command.reply({content:'You do not have permission...',ephemeral:false})
+           })
         }
-};
-client.on('interactionCreate', interaction => {
-    if (interaction.commandName === 'purge') {
-        let amount = interaction.options.getInteger('amount')
-        if (isNaN(amount)) return  interaction.reply({content:'You must specify a number',ephemeral:true});
-        interaction.reply({content:'Purging...',ephemeral:true})
-        interaction.channel.bulkDelete(amount)
-    }
-});
+}

b89fbf4f0654d63e720034f7539d8325d1ba2af6
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2024-08-16 23:18:37 -0500

    Update newmember.js

--- modules/newmember.js
+++ modules/newmember.js
@@ -28,15 +28,15 @@ module.exports = {
                         .setColor('#FFC0CB')
                         .setTitle(`Welcome to ${member.guild.name}, ${member.displayName}!`)
                         .setAuthor({name: client.user.displayName, iconURL: client.user.displayAvatarURL()})
-                        .setDescription('Make sure to read the Rules!')
+                        .setDescription(`Make sure to read the rules <@${member.user.id}>!`)
                         .addFields({name: '\u200B', value: 'Dont forget to checkout the other related server info!'})
                         .setThumbnail(member.guild.iconURL())
                         .setTimestamp()
                 ]
             })
-                .then(embedMessage =>
-                    ['🥳', '🎊', '🎂', '🎉'].forEach(emoji => embedMessage.react(emoji))
-                );
+            
+            .then(embedMessage => ['🥳',/*'🎊', '🎂',*/'🎉'].forEach(emoji => embedMessage.react(emoji), embedMessage.reply(`Make sure to welcome <@${member.user.id}>!`)));
+            
         })
     }
 }
